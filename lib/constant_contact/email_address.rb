da2a3664b2a8781d4eb87f74d69523f148c57ce8
Derek Victory <derekvictory@yahoo.com>
2012-06-23 20:24:04 -0500

    fix email

--- lib/constant_contact/email_address.rb
+++ lib/constant_contact/email_address.rb
@@ -2,5 +2,8 @@
 module ConstantContact
   class EmailAddress < Base
     self.element_name = 'settings/emailaddresses'
+    def self.find_by_email(email_address)
+      find :first, {:params => {:email => email_address.downcase}}
+    end
   end
 end
